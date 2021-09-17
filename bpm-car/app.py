771134621b0232ea8271af600dae195f91270adc
Fabio <4898010+gatto@users.noreply.github.com>
2021-09-17 15:24:10 +0200

    Update app.py

--- bpm-car/app.py
+++ bpm-car/app.py
@@ -4,53 +4,65 @@ import pandas as pd
 
 """
 # Car sale
-This is a representation of a car sale. Emily wants to sell, Eric wants to buy.
-You can choose one of three scenarios. (xxx or enter the data yourself)
 
-`/streamlit_app.py` to customize this app to your heart's desire :heart:
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+## Here are the attributes of my car.
+
+| Hack | Maker | Model | Year | Km | Color |
+|---|---|
+| | Honda | Civic | 2009 | 270°000 | black |
+
+
+## An image of the car
+
+![](https://s.sbito.it/img/86/866cdbc3-555c-4fc5-addd-f2f1f1840ba3/gallery-mobile-1x)
+
+## Initial request
+
+According by [Quattroruote](https://sure) the car's fair price is **700 Euro**.
 """
-scenario1 = pd.DataFrame({"date": [1, 2, 3, 4], "Emily": [10, 20, 30, 40], "Eric": [0, 20]})
 
-st.write(my_df)
+with st.expander('Quattroruote said…'):
+    st.write("«According to calculations on year and km and some secret sauce we concluded the car's fair price is 700 EUR»")
 
-st.table(data.iloc[0:10])
+"""
+## The negotiation log
+"""
 
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
-st.line_chart(chart_data)
+# hack to not display index
+st.markdown("""
+<style>
+table td:nth-child(1) {
+    display: none
+}
+table th:nth-child(1) {
+    display: none
+}
+</style>
+""", unsafe_allow_html=True)
 
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], columns=["lat", "lon"]
-)
+# choosing the dates, same for every scenario
+dates = pd.Series(pd.to_datetime(["2021/8/20", "2021/8/24", "2021/8/25", "2021/8/26"]))
+dates2 = pd.Series(pd.to_datetime(["2021/8/20", "2021/8/24", "2021/8/25", "2021/8/26", "2021/8/26"]))
 
-# st.map(map_data)
 
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
+# inputting the data
+scenario1 = pd.DataFrame({"date": dates, "Emily": ["€ 700", "", "€ 660", ""], "Eric": ["", "€ 500", "", "✅"]})
+scenario2 = pd.DataFrame({"date": dates2, "Emily": ["€ 700", "", "€ 700", "", "❌"], "Eric": ["", "€ 300", "", "€ 350", ""]})
+scenario3 = pd.DataFrame({"date": dates, "Emily": ["", "€ 750", "", "✅"], "Eric": ["€ 680", "", "€ 680", ""]})
+scenarios = [scenario1, scenario2, scenario3]
 
-with st.echo(code_location="below"):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# styling the dates
+for i, _ in enumerate(scenarios):
+    scenarios[i] = scenarios[i].style.format({"date": lambda t: t.strftime("%d-%m-%Y"),})
 
-    Point = namedtuple("Point", "x y")
-    data = []
+# display the goods
+st.sidebar.write("This is a representation of a car sale. Emily wants to sell, Eric wants to buy. You can choose one of three scenarios.")
+choice = st.sidebar.radio("Please select a scenario", ('one', 'two', 'three'))
+if choice == 'one':
+    st.table(scenarios[0])
+elif choice=="two":
+    st.table(scenarios[1])
+elif choice=="three":
+    st.table(scenarios[2])
 
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(
-        alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color="#0068c9", opacity=0.5)
-        .encode(x="x:Q", y="y:Q")
-    )
+st.caption("You can look at the Python code in the github repository: `https://github.com/gatto/viz-experiments/` :heart:")
